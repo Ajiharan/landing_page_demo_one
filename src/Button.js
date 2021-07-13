379109db7e9511888e654f33634f84c603f7a749
Ajiharan <baskaranajiharan1243@gmail.com>
2021-07-13 17:18:42 +0530

    changes

---
+++ src/Button.js
@@ -0,0 +1,23 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import "./Button.scss";
+const STYLES = ["btn--primary", "btn--outline", "btn--test"];
+
+const SIZES = ["btn--medium", "btn--large"];
+const Button = ({ children, type, onClick, buttonStyle, buttonSize }) => {
+  const checkButtonStyle = STYLES.includes(buttonStyle)
+    ? buttonStyle
+    : STYLES[0];
+
+  const checkButtonSize = SIZES.includes(buttonSize) ? buttonSize : SIZES[0];
+
+  return (
+    <Link to="./home" className="btn-mobile">
+      <button className={`btn ${checkButtonStyle} ${checkButtonSize}`}>
+        {children}
+      </button>
+    </Link>
+  );
+};
+
+export default Button;
