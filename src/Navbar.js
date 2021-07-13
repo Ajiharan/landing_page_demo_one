98cc409c3e8b8df6f620c9ab72451c922c9d8580
Ajiharan <baskaranajiharan1243@gmail.com>
2021-07-13 18:05:37 +0530

    navbar style updated

--- src/Navbar.js
+++ src/Navbar.js
@@ -11,40 +11,41 @@ const Navbar = () => {
     <React.Fragment>
       <div className="navbar">
         <div className="navbar-container">
-          <Link to="./home">PHCY</Link>
-          <i className="fas fa-camera-retro"></i>
+          <Link to="./home" className="navbar-logo">
+            PHCY <i className="fas fa-camera-retro"></i>
+          </Link>
+          <div className="menu-icon">
+            <i className={click ? "fas fa-times" : "fas fa-bars"} />
+          </div>
+          <ul className={click ? "nav-menu active" : "nav-menu"}>
+            <li className="nav-item">
+              <Link to="./home" className="nav-links">
+                Home
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="./home" className="nav-links">
+                Services
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="./home" className="nav-links">
+                Products
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="./home" className="nav-links">
+                Offers
+              </Link>
+            </li>
+            <li className="nav-item">
+              <Link to="./home" className="nav-links-mobile">
+                Sign Up
+              </Link>
+            </li>
+          </ul>
+          {isButtonShow && <Button buttonStyle="btn--outline">SIGNUP</Button>}
         </div>
-        <div className="menu-icon">
-          <i className={click ? "fas fa-times" : "fas fa-bars"} />
-        </div>
-        <ul className={click ? "nav-menu active" : "nav-menu"}>
-          <li className="nav-item">
-            <Link to="./home" className="nav-links">
-              Home
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="./home" className="nav-links">
-              Services
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="./home" className="nav-links">
-              Products
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="./home" className="nav-links">
-              Offers
-            </Link>
-          </li>
-          <li className="nav-item">
-            <Link to="./home" className="nav-links-mobile">
-              Sign Up
-            </Link>
-          </li>
-        </ul>
-        {isButtonShow && <Button buttonStyle="btn--outline">SIGNUP</Button>}
       </div>
     </React.Fragment>
   );
