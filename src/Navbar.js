510faad317871c643cc21472ce96b702ee7580d1
Ajiharan <baskaranajiharan1243@gmail.com>
2021-07-14 18:32:11 +0530

    fixed issues

--- src/Navbar.js
+++ src/Navbar.js
@@ -6,45 +6,64 @@ const Navbar = () => {
   const [click, setClick] = useState(false);
   const [isButtonShow, setButtonShow] = useState(true);
 
-  useEffect(() => {}, []);
+  const closeMobileBtn = () => {
+    setClick(false);
+  };
+
+  const showButton = () => {
+    if (window.innerWidth <= 970) {
+      setButtonShow(false);
+    } else {
+      setButtonShow(true);
+    }
+  };
+
+  useEffect(() => {
+    showButton();
+  }, []);
+
+  const handleClick = () => setClick(!click);
+
+  window.addEventListener("resize", showButton);
+
   return (
     <React.Fragment>
       <div className="navbar">
         <div className="navbar-container">
-          <Link to="./home" className="navbar-logo">
+          <Link to="./home" className="navbar-logo" onClick={closeMobileBtn}>
             PHCY <i className="fas fa-camera-retro"></i>
           </Link>
-          <div className="menu-icon">
+          <div className="menu-icon" onClick={handleClick}>
             <i className={click ? "fas fa-times" : "fas fa-bars"} />
           </div>
           <ul className={click ? "nav-menu active" : "nav-menu"}>
             <li className="nav-item">
-              <Link to="./home" className="nav-links">
+              <Link to="./home" className="nav-links" onClick={closeMobileBtn}>
                 Home
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item" onClick={closeMobileBtn}>
               <Link to="./home" className="nav-links">
                 Services
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item" onClick={closeMobileBtn}>
               <Link to="./home" className="nav-links">
                 Products
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item" onClick={closeMobileBtn}>
               <Link to="./home" className="nav-links">
                 Offers
               </Link>
             </li>
-            <li className="nav-item">
+            <li className="nav-item" onClick={closeMobileBtn}>
               <Link to="./home" className="nav-links-mobile">
                 Sign Up
               </Link>
             </li>
           </ul>
-          {isButtonShow && <Button buttonStyle="btn--outline">SIGNUP</Button>}
+          {isButtonShow && <Button buttonStyle="btn--outline">SIGN UP</Button>}
         </div>
       </div>
     </React.Fragment>
